fb36def97d20f6282873fb2b58b94189453bd1a3
Torin Lahiru <torinlahiru@gmail.com>
2025-04-11 09:09:48 +0530

    bootstrap is added

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" >
     <title>Student Management System</title>
 </head>
 <body>
 <h1>Hello</h1>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" ></script>
 </body>
 </html>
