f0a3a3f4c57b56de771d693ebf8aabe3ce8107af
Torin Lahiru <torinlahiru@gmail.com>
2025-04-13 11:10:48 +0530

    student model updated

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    //
+    protected $table = 'students';
+    protected $primaryKey = 'id';
+    protected $fillable = ['name', 'address', 'mobile'];
+    use HasFactory;
 }
