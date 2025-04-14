53436f566bb5f94036d5be42130e02f8eb5f11fe
Torin Lahiru <torinlahiru@gmail.com>
2025-04-14 21:36:30 +0530

    submit btn added

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -8,11 +8,12 @@
             <form action="{{url('student')}}" method="POST">
                 @csrf
                 <label for="Name">Name</label>
-                <input type="text" name="Name" id="">
+                <input type="text" name="Name" id="" class="form-control">
                 <label for="Address">Address</label>
-                <input type="text" name="Address">
+                <input type="text" name="Address" class="form-control">
                 <label for="Mobile">Mobile</label>
-                <input type="text" name="Mobile">
+                <input type="text" name="Mobile" class="form-control">
+                <button type="submit" class="btn btn-primary">Submit</button>
             </form>
         </div>
     </div>
