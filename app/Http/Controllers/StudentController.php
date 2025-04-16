0596f11d52442cfba3d116850b7c6ff71ed7ee75
Torin Lahiru <torinlahiru@gmail.com>
2025-04-16 23:08:00 +0530

    student controller update with show

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -47,7 +47,8 @@ class StudentController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $student = Student::find($id);
+        return view('students.show')->with('student', $student);
     }
 
     /**
